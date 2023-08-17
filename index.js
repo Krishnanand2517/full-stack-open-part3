b4808115fa54371967f107d2df653f72a8ee114e
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-08-17 22:01:58 +0530

    Added frontend to the application

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('content', (request, response) => JSON.stringify(request.body));
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 
 let persons = [
