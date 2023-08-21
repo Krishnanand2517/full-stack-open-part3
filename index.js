4ec22cb51ff675f84119a281b83a2a6c4b65106c
Krishnanand Yadav <krishnanand2517@gmail.com>
2023-08-21 22:31:31 +0530

    Done exercise 3c

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
+const Person = require('./models/person');
 
 morgan.token('content', (request, response) => JSON.stringify(request.body));
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: "malformatted id" });
+    }
+
+    next(error);
+};
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
+app.use(errorHandler);
 
 let persons = [
     { 
@@ -34,41 +47,58 @@ let persons = [
 ];
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({}).then(persons => {
+        response.json(persons);
+    });
 });
 
 app.get('/info', (request, response) => {
     const currentDate = new Date().toLocaleString();
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-    response.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${currentDate} ${timeZone}</p>
-    `);
+    Person.find({})
+        .then(persons => {
+            response.send(`
+                <p>Phonebook has info for ${persons.length} people</p>
+                <p>${currentDate} ${timeZone}</p>
+            `);
+        });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(p => p.id === id);
-    
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(p => p.id !== id);
-
-    response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
-const generateId = () => {
-    const id = Math.ceil(Math.random() * 1000);
-    return id;
-};
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        phone: body.phone
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson);
+        })
+        .catch(error => next(error));
+});
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
@@ -91,18 +121,17 @@ app.post('/api/persons', (request, response) => {
         });
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
-        phone: body.phone,
-        id: generateId()
-    };
+        phone: body.phone
+    });
 
-    persons = persons.concat(person);
-
-    response.json(person);
+    person.save().then(savedPerson => {
+        response.json(savedPerson);
+    });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+    console.log("Give password as argument");
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@project0.hxmahgt.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    phone: String
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+if (process.argv.length === 3) {
+    Person.find({}).then(result => {
+        console.log("Phonebook:");
+        result.forEach(person => {
+            console.log(person.name, person.phone);
+        });
+
+        mongoose.connection.close();
+        process.exit(1);
+    });
+}
+else if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        phone: process.argv[4]
+    });
+    
+    person.save().then(result => {
+        console.log(`Added ${person.name} number ${person.phone} to the phonebook`);
+        mongoose.connection.close();
+    });
+}
